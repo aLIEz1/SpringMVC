47908297a0eeb47d6812a5652e0b30e7e042aea6
super <super1742720898@gmail.com>
2020-07-20 16:08:10 +0800

    Initial commit

---
+++ SpringMVC-Hello/src/main/java/com/lm/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.lm.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg","helloSpringMVC");
+        modelAndView.setViewName("hello");
+        return modelAndView;
+    }
+}
